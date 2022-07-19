9cf1ba4d30c22b53882107e27137a25d309da8c2
root <nguonpheng@shinhan.com>
2022-07-19 09:14:33 +0700

    Create stationery migration

---
+++ database/migrations/2022_07_19_084554_create_stationeries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stationeries', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('quantity');
+            $table->integer('stock')->default(0);
+            $table->string('remark')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stationeries');
+    }
+};
---
+++ database/migrations/2022_07_19_090152_create_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('request_by');
+            $table->string('ip_address');
+            $table->integer('department_id');
+            $table->integer('stationery_id');
+            $table->integer('quantity');
+            $table->timestamp('approved_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
